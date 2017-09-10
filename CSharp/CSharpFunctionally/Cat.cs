c55cc471e0ce6457d6b519295d221030900dc78f
Conny Sjögren <ConnyS@intergen.org.nz>
2017-09-11 09:45:05 +1200

    updated gitignore

--- CSharp/CSharpFunctionally/Cat.cs
+++ CSharp/CSharpFunctionally/Cat.cs
@@ -5,6 +5,107 @@ using MoreLinq;
 
 namespace CSharpFunctionally
 {
+    public class NonFunctionalCat
+    {
+        public static List<string> PopularCatNamesAustralia = new List<string>
+        {
+            "Ashes",
+            "Tiger",
+            "Puss",
+            "Smokey",
+            "Misty",
+            "Tigger",
+            "Kitty",
+            "Oscar",
+            "Missy",
+            "Max",
+            "Ginger",
+        };
+        public static List<string> PopularCatNamesUK = new List<string>
+        {
+            "Ashes",
+            "Molly",
+            "Charlie",
+            "Tigger",
+            "Poppy",
+            "Oscar",
+            "Smudge",
+            "Millie",
+            "Daisy",
+            "Max",
+            "Jasper",
+            "Trevor",
+        };
+        public static List<string> PopularCatNamesGermany = new List<string>
+        {
+            "Ashes",
+            "Felix",
+            "Minka",
+            "Moritz",
+            "Charly",
+            "Tiger",
+            "Eve",
+            "Susi",
+            "Lisa",
+            "Blacky",
+            "Muschi",
+        };
+        public static List<string> PopularCatNamesCanada = new List<string>
+        {
+            "Ashes",
+            "Minou",
+            "Grisou",
+            "Ti-Mine",
+            "Félix",
+            "Caramel",
+            "Mimi",
+            "Pacha",
+            "Charlotte",
+            "Minette",
+            "Chanel",
+        };
+
+        public static List<string> DistinctPopularNames = new List<string>();
+
+        private static Random random = new Random();
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        public NonFunctionalCat(string name)
+        {
+            var randomIndex = random.Next(0, ReferenceData.Guids.Count);
+            var randomGuidString = ReferenceData.Guids[randomIndex];
+            var randomGuid = Guid.Parse(randomGuidString);
+
+            Id = randomGuid;
+            Age = 0;
+            Name = name;
+        }
+
+        public static Cat GenerateCat()
+        {
+            var randomIndex = random.Next(0, ReferenceData.Guids.Count);
+            var randomGuidString = ReferenceData.Guids[randomIndex];
+            var randomGuid = Guid.Parse(randomGuidString);
+            
+            return new Cat
+            {
+                Age = random.Next(0, 15),
+                Id = ReferenceData.Guids.RandomSubset(1).Select(s => Guid.Parse(s)).First(),
+                Name = DistinctPopularNames.RandomSubset(1).First()
+            };
+        }
+
+        public static List<Cat> GenerateCats(int number)
+        {
+            return Enumerable.Range(0, number)
+                .Select(i => GenerateCat())
+                .ToList();
+        }
+    }
+
     public class Cat
     {
         public static List<string> PopularCatNamesAustralia = new List<string>
